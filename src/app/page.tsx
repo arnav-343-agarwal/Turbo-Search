b477982350edd2608cbf6d8c2e6005c20928f621
Arnav Agarwal <agarwalarnav2003@gmail.com>
2025-03-01 18:10:27 +0530

    5-url issue fixed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -21,7 +21,7 @@ export default function Home() {
     const fetchData = async () => {
       if (!input) return setSearchResults(undefined)
 
-      const res = await fetch(`https://turbosearch.turbosearch.workers.dev//api/search?q=${input}`)
+      const res = await fetch(`https://turbosearch.turbosearch.workers.dev/api/search?q=${input}`)
       const data = (await res.json()) as { results: string[]; duration: number }
       setSearchResults(data)
     }
